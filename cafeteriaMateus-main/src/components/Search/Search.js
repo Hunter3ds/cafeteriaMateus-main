4d2197b174cf28de607b1d910da41a0059ef1cbc
Hunter3ds <ghostmat817@gmail.com>
2024-01-29 16:02:10 -0300

    cafeteria-MateusMain

---
+++ cafeteriaMateus-main/src/components/Search/Search.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import './seacrh.css';
+import Loader from './components';
+import Logo from './culinaaria.png';
+
+export default function Search(){
+    const [searchTerm, setSearchTerm] = useState('')
+    const [searchResults, setSearchResults] = useState([])
+    const [showPopup, setShowPopoup] = useState(false);
+    const [carregando, setCarregando] = useState(false);
+
+    // codigo JavaScript
+    const handleSearch = async ()=>{
+        try{
+            setCarregando(true);
+            const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+
+            if(response.data.meals=== null || response.data.meals === 0){
+                console.log('nenhum resultado foi encontrado');
+                setSearchResults([]);
+                setShowPopoup(true);
+
+            } else{
+                console.log('Resultados encontrados');
+                setSearchResults(response.data.meals)
+                setShowPopoup(false);
+                
+            }
+        }
+        catch(error){
+            console.error('Erro ao extrair os dados', error)
+        }
+        finally{
+            setCarregando(false)
+        }
+        }
+   
+    return(
+        <>
+        <header classNmae='container'>
+            <div className='information-search'>
+                <span className='search-input'>
+                    <label className='label-input'>Pesquisar</label>
+                    <input type='text' className='search-input'
+                    id='search-term'
+                    value = {searchTerm}
+                    onChange={(e)=>{setSearchTerm(e.target.value)}}/>
+                </span>
+                <button type='button' className='search-button' onClick={handleSearch} >PESQUISAR</button>
+            </div>
+        </header>
+        <div className='content-container'>
+            <div className='answerImages'>
+                {carregando?(<Loader/>): searchResults.lengh === 0 ?
+                 (<p> Nenhum resultado encontrado</p>): (searchResults.map((recipe)=>{
+
+                 }) )}
+            </div>
+        </div>
+
+        </>
+    )
+}
+
